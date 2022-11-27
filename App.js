7df86d596f67ce26310c7f81fe6aa87f6340bcbc
2uyetvippro <111883726+2uyetvippro@users.noreply.github.com>
2022-11-27 11:27:17 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import RootCompenent from './src/view/index'
+
+export default function App() {
+  return (
+      <RootCompenent />
+  );
+}
